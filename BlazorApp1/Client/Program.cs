c0f36b0a4947baf02ac7a4251803581d6cb39fe0
unknown <thinhbka.nguyenvan@gmail.com>
2023-07-22 13:25:17 +0900

    updated code

--- BlazorApp1/Client/Program.cs
+++ BlazorApp1/Client/Program.cs
@@ -1,6 +1,8 @@
 using BlazorApp1;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
 
 namespace BlazorApp1
 {
@@ -11,7 +13,7 @@ namespace BlazorApp1
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
+            builder.Services.AddMudServices();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
